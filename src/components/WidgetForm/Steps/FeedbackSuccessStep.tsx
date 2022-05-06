03f8808f2dc12b24dae584189ad3a0eb7781d1dc
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2022-05-05 22:00:13 -0300

    feat: add success feedback

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,3 +1,33 @@
-export function FeedbackSuccessStep() {
-  return ();
+import { CloseButton } from "../../CloseButton";
+import successUrl from "../../../assets/success.svg";
+
+interface FeedbackSuccessStepProps {
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({ onFeedbackRestartRequested }: FeedbackSuccessStepProps) {
+  return (
+    <>
+      <header>
+        <CloseButton />
+      </header>
+
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img
+          src={successUrl}
+          alt="imagem de sucesso"
+        />
+
+        <span className="text-xl mt-2">Agradecemos o feedback!</span>
+
+        <button
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+          onClick={onFeedbackRestartRequested}
+        >
+          Quero enviar outro
+        </button>
+
+      </div>
+    </>
+  );
 }
