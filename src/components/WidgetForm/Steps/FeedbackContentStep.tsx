32ebe87f5180a94e66d4dcccffd51cd02fc5bebb
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2022-05-07 21:41:09 -0300

    feat: integrage with api

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,33 +1,45 @@
-import { ArrowLeft, Camera } from "phosphor-react";
 import { FormEvent, useState } from "react";
+import { ArrowLeft } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
-import { CloseButton } from "../../CloseButton";
+import { api } from "../../../lib/api";
+import { CloseButton } from "../../CloseButton"
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
-interface FeedbackContentStepProps {
+interface FeedbackTypeStepProps {
   feedbackType: FeedbackType;
-  onBackPress: () => void;
+  onFeedbackRestartRequested: () => void;
   onFeedbackSent: () => void;
 }
 
 export function FeedbackContentStep({
   feedbackType,
-  onBackPress,
+  onFeedbackRestartRequested,
   onFeedbackSent,
-}: FeedbackContentStepProps) {
-  const [screenshot, setScreenshot] = useState<string | null>(null);
-  const [comment, setComment] = useState('');
+}: FeedbackTypeStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [comment, setComment] = useState('')
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
-  const feedbackTypeInfos = feedbackTypes[feedbackType];
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
-      screenshot,
-      comment,
-    })
+    setIsSendingFeedback(true);
 
+    // console.log({
+    //   screenshot,
+    //   comment,
+    // })
+
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot,
+    });
+
+    setIsSendingFeedback(false);
     onFeedbackSent();
   }
 
@@ -37,18 +49,14 @@ export function FeedbackContentStep({
         <button
           type="button"
           className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
-          onClick={onBackPress}
+          onClick={onFeedbackRestartRequested}
+          title="Voltar"
         >
           <ArrowLeft weight="bold" className="w-4 h-4" />
         </button>
-
         <span className="text-xl leading-6 flex items-center gap-2">
-          <img
-            src={feedbackTypeInfos.image.source}
-            alt={feedbackTypeInfos.image.alt}
-            className="w-6 h-6"
-          />
-          {feedbackTypeInfos.title}
+          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+          {feedbackTypeInfo.title}
         </span>
 
         <CloseButton />
@@ -56,9 +64,9 @@ export function FeedbackContentStep({
 
       <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
         <textarea
-          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar-thumb-zinc-700 scrollbar-transparent scrollbar-thin"
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
-          onChange={event => setComment(event.target.value)}
+          onChange={(event) => setComment(event.target.value)}
         />
 
         <footer className="flex gap-2 mt-2">
@@ -69,13 +77,14 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
+            disabled={comment.length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
-            disabled={!comment}
           >
-            Enviar feedback
+            {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
           </button>
         </footer>
+
       </form>
     </>
-  );
+  )
 }
